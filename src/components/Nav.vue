ac6e4aa9fe924dc0b27048ad2ea1c7daf3b115ff
Topan Alfa <topan.alfa@gmail.com>
2020-02-14 16:38:19 +0700

    add new firebase configuration and Navigation menu

---
+++ src/components/Nav.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">Rent Car</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">
+              Home
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {};
+</script>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDbIr8z7L3ZZvEsQddWoWdfEqKpZrCdqL8",
+  authDomain: "rentcar-b0e8b.firebaseapp.com",
+  databaseURL: "https://rentcar-b0e8b.firebaseio.com",
+  projectId: "rentcar-b0e8b",
+  storageBucket: "rentcar-b0e8b.appspot.com",
+  messagingSenderId: "651171347319",
+  appId: "1:651171347319:web:3a1a4520cd0657e54a3f72"
+};
