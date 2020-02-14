33363c0790b2b5449ecc3c34d157494ad7efc8fd
Topan Alfa <topan.alfa@gmail.com>
2020-02-14 17:18:45 +0700

    setup route app

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "@/components/Dashboard";
+import BookCar from "@/components/BookCar";
+import AddCar from "@/components/AddCar";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "dashboard",
+      component: Dashboard
+    },
+    {
+      path: "/add",
+      name: "add-car",
+      component: AddCar
+    },
+    {
+      path: "/book/:car_id",
+      name: "book-car",
+      component: BookCar
     }
   ]
-})
+});
